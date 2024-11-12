c72a495d37adab14002dac70f2e29da336080cfc
BerkayUnutkanEHB <berkay.unutkan@student.ehb.be>
2024-11-12 10:26:16 +0100

    Initial commit

---
+++ VideoEditorApp/VideoEditorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEditorAppApp.swift
+//  VideoEditorApp
+//
+//  Created by Berkay Unutkan on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoEditorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
